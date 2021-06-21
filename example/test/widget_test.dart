3a9295496480296006e3ec7a2fbb0a6d055bae67
F <rocaforarnelo2@gmail.com>
2021-06-22 00:25:28 +0800

    Added example project

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:time_of_day_widget/time_of_day_widget.dart';
+import 'package:example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
